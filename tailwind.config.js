576772dd95311daabb40679e2c5718e193b240d5
ClaudiusAyadi <ayaclaudius@gmail.com>
2023-09-04 21:55:24 +0100

    Extended: font-family, and base color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        base: ["Galano Grotesque Alt", "sans-serif"],
+      },
+      colors: {
+        base: "#2b2b2b",
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
 };
