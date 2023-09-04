4524f8a673316e1170ca515c6e93a0e5d43c6004
ClaudiusAyadi <ayaclaudius@gmail.com>
2023-09-04 23:51:32 +0100

    Added custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      tablet: "992px",
+    },
     extend: {
       fontFamily: {
         base: ["Galano Grotesque Alt", "sans-serif"],
